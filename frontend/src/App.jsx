bddf43276198175f2430246fd41c6e1cf2bb244d
Arjun.O <arjundlm10@gmail.com>
2026-06-03 11:50:56 +0530

    fix: Removed host related Link and Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Venue from './pages/Venue';
 import Category from './pages/Category';
-import Host from './pages/Host';
 import Signin from './pages/Signin';
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/venue' element={<Venue/>}/>
         <Route path='/category' element={<Category/>}/>
-        <Route path='/host' element={<Host/>}/>
         <Route path='/signin' element={<Signin/>}/>
       </Routes>
     </div>
